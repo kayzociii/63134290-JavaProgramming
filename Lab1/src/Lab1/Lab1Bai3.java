ecb43b5bfe4c14f5ee119710d7cf81d8fb48f29b
Nguyen Khang <kayzociii@gmail.com>
2024-01-21 16:26:32 +0700

    Update Bai3

--- Lab1/src/Lab1/Lab1Bai3.java
+++ Lab1/src/Lab1/Lab1Bai3.java
@@ -12,7 +12,6 @@ public class Lab1Bai3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhập vào độ dài cạnh: ");
 		double canh = scanner.nextDouble();	
