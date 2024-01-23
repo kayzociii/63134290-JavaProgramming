c167b09f1ae5adf56706b55c0d64c89953a240b2
Nguyen Khang <kayzociii@gmail.com>
2024-01-23 16:28:37 +0700

    Create Lab2

---
+++ BaiTapJava/src/Lab2/Lab2Bai1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lab2;
+
+/**
+ * 
+ */
+public class Lab2Bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
