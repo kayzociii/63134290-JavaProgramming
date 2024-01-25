664064f95876963950449af3288b9a0a66220775
Nguyen Khang <kayzociii@gmail.com>
2024-01-25 16:17:36 +0700

    Update Lab3Bai2

--- BaiTapJava/src/Lab3/Lab3Bai2.java
+++ BaiTapJava/src/Lab3/Lab3Bai2.java
@@ -12,8 +12,11 @@ public class Lab3Bai2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x = 8;
+		for(int i=1; i<=10;i++){
+		System.out.printf("%d x %d = %d", x, i, x*i);
+		System.out.println();
+		}
 	}
 
 }
