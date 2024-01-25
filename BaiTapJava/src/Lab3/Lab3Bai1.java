5f6cd4b7bc6ec607c4f5dba7abb74d019ab9442b
Nguyen Khang <kayzociii@gmail.com>
2024-01-25 15:36:48 +0700

    Create Lab3
    Tạo Lab3

---
+++ BaiTapJava/src/Lab3/Lab3Bai1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lab3;
+
+/**
+ * 
+ */
+public class Lab3Bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
