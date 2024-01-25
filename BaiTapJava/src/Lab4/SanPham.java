c6bd0f84020dc6c2af9a573d2f3dc98024d6ddec
Nguyen Khang <kayzociii@gmail.com>
2024-01-25 18:12:14 +0700

    Create Lab4
    Tạo Lab4

---
+++ BaiTapJava/src/Lab4/SanPham.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lab4;
+
+/**
+ * 
+ */
+public class SanPham {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
