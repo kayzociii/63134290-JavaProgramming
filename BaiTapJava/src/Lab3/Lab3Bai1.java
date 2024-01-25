0592348eb9c927b0a8ea2e2d9fb93507b847c077
Nguyen Khang <kayzociii@gmail.com>
2024-01-25 15:44:23 +0700

    Update Lab3Bai1

--- BaiTapJava/src/Lab3/Lab3Bai1.java
+++ BaiTapJava/src/Lab3/Lab3Bai1.java
@@ -2,7 +2,7 @@
  * 
  */
 package Lab3;
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -12,8 +12,30 @@ public class Lab3Bai1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Nhập một số nguyên: ");
+        int N = scanner.nextInt();
+
+        boolean ok = true; 
+        
+        if (N <= 1) {
+            ok = false;
+        }
+
+        for (int i = 2; i < N-1; i++) {
+            if (N % i == 0) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) {
+            System.out.println(N + " là số nguyên tố.");
+        } else {
+            System.out.println(N + " không phải là số nguyên tố.");
+        }
 	}
+
 
 }
