41af4c1888e458459c680fb46998882a64fa01f2
Nguyen Khang <kayzociii@gmail.com>
2024-01-25 16:11:54 +0700

    Update Lab3Bai2

---
+++ BaiTapJava/src/Lab3/Lab3Bai2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lab3;
+
+/**
+ * 
+ */
+public class Lab3Bai2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
