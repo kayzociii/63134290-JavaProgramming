13f574c388531c214708417b9951406b5d5a93de
Nguyen Khang <kayzociii@gmail.com>
2024-02-18 21:56:48 +0700

    Update Lab4Bai3
    Update

--- BaiTapJava/src/Lab4/Lab4.java
+++ BaiTapJava/src/Lab4/Lab4.java
@@ -14,13 +14,16 @@ class SanPham {
 	public SanPham() {
 		
 	}
+	public SanPham(String tenSp, double donGia) {
+		this(tenSp, donGia, 0);
+	}
 	public SanPham(String tenSp, double donGia, double giamGia) {
 		this.tenSp = tenSp;
 		this.donGia = donGia;
 		this.giamGia = giamGia;
 	}
 	
-	public double getThueNhapKhau() {
+	private double getThueNhapKhau() {
 		return 0.1 * donGia;
 	}
 	
@@ -46,6 +49,8 @@ public class Lab4 {
 	public static void main(String[] args) {
 		SanPham sp1 = new SanPham();
 		SanPham sp2 = new SanPham();
+		SanPham sp3 = new SanPham("A", 20000, 10000);
+		SanPham sp4 = new SanPham("B", 20000);
 		System.out.println("Nhập vào thông tin sản phẩm 1: ");
 		sp1.nhap();
 		System.out.println("Nhập vào thông tin sản phẩm 2: ");
@@ -54,6 +59,10 @@ public class Lab4 {
 		sp1.xuat();
 		System.out.println("Sản phẩm 2: ");
 		sp2.xuat();
+		System.out.println("Sản phẩm 3: ");
+		sp3.xuat();
+		System.out.println("Sản phẩm 4: ");
+		sp4.xuat();
 	}
 }
 
