42d463339757e59986e52fab5f081ddaac96fa73
Nguyen Khang <kayzociii@gmail.com>
2024-02-22 19:35:17 +0700

    Update Lab6Bai1
    Update lần 1

--- BaiTapJava/src/Lab6/Lab6Bai1.java
+++ BaiTapJava/src/Lab6/Lab6Bai1.java
@@ -1,10 +1,50 @@
 package Lab6;
+import java.util.Scanner;
 
 public class Lab6Bai1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập vào họ và tên: ");
+		String hoten = scanner.nextLine();
+		
+		String ho = LayHo(hoten);
+		String tendem = LayTenDem(hoten);
+		String ten = LayTen(hoten);
+		
+		System.out.println("Họ: " + ho);
+		System.out.println("Tên đệm: " + tendem);
+		System.out.println("Tên: " + ten);
 	}
-
+	
+	public static String LayHo(String hoten) {
+		int index = hoten.indexOf(' ');
+		if(index != -1) {
+			return hoten.substring(0, index);
+		}
+		else {
+			return hoten;
+		}	
+	}
+	
+	public static String LayTen(String hoten) {
+		int index = hoten.lastIndexOf(' ');
+		if(index != -1) {
+			return hoten.substring(index + 1);
+		}
+		else {
+			return hoten;
+		}
+	}
+	
+	public static String LayTenDem(String hoten) {
+		int index = hoten.indexOf(' ');
+		int index2 = hoten.lastIndexOf(' ');
+		if(index != -1 && index2 != -1 && index < index2) {
+			return hoten.substring(index + 1, index2);
+		}
+		else {
+			return hoten;
+		}
+	}
 }
