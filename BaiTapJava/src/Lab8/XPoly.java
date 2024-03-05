027017d3a9db0e1bc62242cd90cf687aa56e752b
Nguyen Khang <kayzociii@gmail.com>
2024-03-05 21:15:04 +0700

    Update Lab8Bai1
    Update

--- BaiTapJava/src/Lab8/XPoly.java
+++ BaiTapJava/src/Lab8/XPoly.java
@@ -1,5 +1,11 @@
 package Lab8;
 
 public class XPoly {
-
+	public static final double sum(double... x) {
+        double kq = 0;
+        for (double num : x) {
+            kq += num;
+        }
+        return kq;
+    }
 }
